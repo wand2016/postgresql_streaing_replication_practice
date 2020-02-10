cdc048bda294744e77fef2c9eda1bdf9198a9b87
Daiki Horiyama <d19921207@gmail.com>
2020-02-11 00:12:33 +0900

    同期レプリケーション設定
    - standbyの`application_name`設定がなぜか効かない
    - ので、いったんmasterの`synchronous_standby_names`を`*`に
    - ログいっぱい出すように

--- docker/master/docker-entrypoint-initdb.d/sample.sh
+++ docker/master/docker-entrypoint-initdb.d/sample.sh
@@ -12,6 +12,14 @@ host    replication     replication_user 0.0.0.0/0     trust
 host    replication     replication_user 0.0.0.0/0     trust
 EOF
 
+cat >> postgresql.conf <<EOF
+synchronous_standby_names = '*'
+log_connections = on
+log_disconnections = on
+log_replication_commands = on
+EOF
+
+
 pg_ctl reload
 
 # ensure that hba settings are applied
